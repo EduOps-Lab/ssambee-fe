3157354da116cee70fb735fa2258efae6e23c42d
yoorrll <lyl1567@naver.com>
2026-01-26 23:53:57 +0900

    feat: modal-provider 적용

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -4,12 +4,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 
+import { ModalProvider } from "./ModalProvider";
+
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
-      {children}
+      <ModalProvider>{children}</ModalProvider>
     </QueryClientProvider>
   );
 }
